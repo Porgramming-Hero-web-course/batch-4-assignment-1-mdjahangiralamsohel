ef21a400bcb7f858d5fa372ab794f8e3fd9f5473
mdjahangiralamsohel <mdjahangiralam6030@gmail.com>
2024-11-08 04:21:23 +0600

    assignment-1

--- dist/problem-5.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-{
-    const getProperty = (obj, property) => {
-        const { [property]: value } = obj;
-        return value;
-    };
-    const person = { name: "Alice", age: 30 };
-    console.log(getProperty(person, "name"));
-}
